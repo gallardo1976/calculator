8b862d72d6b9d63def8c90fdb84e4e115dc11af7
gallardo1976 <brayan.gallardo@correounivalle.edu.co>
2024-03-26 13:59:49 -0500

    añadir

--- src/app/componentes/calculadora.jsx
+++ src/app/componentes/calculadora.jsx
@@ -23,118 +23,118 @@ const Calculator = () => {
   };
 
   return (
-    <div className="bg-gray-200 rounded p-4 w-64 mx-auto mt-10">
+    <div className="bg-gradient-to-br from-gray-900 to-gray-700 bg-opacity-90 rounded-lg p-4 w-72 mx-auto mt-10 shadow-lg">
       <input
         type="text"
         value={expression}
         disabled
-        className="w-full mb-2 p-2 border rounded text-black border-cyan-950"
+        className="w-full mb-2 p-2 font-bold border rounded text-gray-800 bg-gray-300 border-transparent focus:border-gray-500 focus:bg-white focus:ring-gray-500"
       />
       <div className="grid grid-cols-4 gap-2">
         <button
           onClick={clearExpression}
-          className="col-span-3 bg-red-500 text-white p-2 rounded"
+          className="col-span-3 bg-red-800 hover:bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           C
         </button>
         <button
           onClick={() => handleClick("/")}
-          className="bg-blue-500 text-white p-2 rounded"
+          className="bg-gray-800 hover:bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           /
         </button>
         <button
           onClick={() => handleClick("*")}
-          className="bg-blue-500 text-white p-2 rounded"
+          className="bg-gray-800 hover:bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           *
         </button>
         <button
           onClick={() => handleClick("7")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           7
         </button>
         <button
           onClick={() => handleClick("8")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           8
         </button>
         <button
           onClick={() => handleClick("9")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           9
         </button>
         <button
           onClick={() => handleClick("+")}
-          className="bg-blue-500 text-white p-2 rounded"
+          className="bg-gray-800 hover:bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           +
         </button>
         <button
           onClick={() => handleClick("4")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           4
         </button>
         <button
           onClick={() => handleClick("5")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           5
         </button>
         <button
           onClick={() => handleClick("6")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           6
         </button>
         <button
           onClick={() => handleClick("-")}
-          className="bg-blue-500 text-white p-2 rounded"
+          className="bg-gray-800 hover:bg-gray-900 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           -
         </button>
         <button
           onClick={() => handleClick("1")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           1
         </button>
         <button
           onClick={() => handleClick("2")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           2
         </button>
         <button
           onClick={() => handleClick("3")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           3
         </button>
         <button
           onClick={calculateResult}
-          className="bg-green-500 text-white p-2 rounded col-span-1"
+          className="bg-gray-900 hover:bg-gray-800 text-white p-3 rounded-lg col-span-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           =
         </button>
         <button
           onClick={() => handleClick("0")}
-          className="bg-gray-500 text-white p-2 rounded col-span-2"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg col-span-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           0
         </button>
         <button
           onClick={() => handleClick(".")}
-          className="bg-gray-500 text-white p-2 rounded"
+          className="bg-gray-700 hover:bg-gray-800 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
         >
           .
         </button>
-        <div className="col-span-4 flex justify-center text-xs">
-          <span className="text-gray-500">Gallardo2024</span>
+        <div className="col-span-4 flex justify-center text-sm text-gray-500">
+          <span>Gallardo2024</span>
         </div>
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,21 @@ import Calculator from "./componentes/calculadora";
 const Home = () => {
   return (
     <div className="container">
+      <div class="flex justify-center h-screen">
+        <div class="bg-gray-800 rounded-lg shadow-lg p-8 mx-4 sm:mx-auto max-w-md w-full">
+          <h2 class="text-2xl font-semibold text-white mb-4">
+            Mi Div Centrado Horizontalmente
+          </h2>
+          <p class="text-gray-300 mb-6">
+            Este es un ejemplo de un div centrado horizontalmente y moderno
+            creado con Tailwind CSS.
+          </p>
+          <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
+            Botón de Ejemplo
+          </button>
+        </div>
+      </div>
+
       <Calculator />
     </div>
   );
